5968f3f670922b09ad6b8d8abee3218070307ad3
DzhumagazievaDiana <113889448+DzhumagazievaDiana@users.noreply.github.com>
2023-03-03 23:33:45 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Lab7 : Form
+    {
+        public Lab7()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            double sum = 0;
+            double summand = 0.0;
+            double x;
+            bool f = false;
+            double precision;
+            if (double.TryParse(txtPrecision.Text, out precision))
+            {
+                f = true;
+            }
+            do
+            {
+                counter++;
+                if (double.TryParse(txtX.Text, out x))
+                {
+                    summand = double.Parse(txtX.Text) / counter;
+                }
+                sum += summand;
+            } while (f && Math.Abs(summand) > precision);
+            if (f)
+            {
+                lblResult.Text = "сумма = " + sum + ", количество = " + counter;
+
+            }
+            else
+            {
+                MessageBox.Show("Введите числовое значение!");
+            }
+        }
+    }
+}
